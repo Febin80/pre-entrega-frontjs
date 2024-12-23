40254480cfee660a937fae809e7b8ea0f24d38d3
Febin80 <kevinkuo@hotmail.com.ar>
2024-12-23 09:55:25 -0300

    tercer cambio

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const logo=document.getElementById("log");
 const inicio1=document.getElementById("inicio");
 const form = document.getElementById('contactForm');
-const boton = document.getElementById("enviar");
 const newForm1=document.getElementById("Newsletter-form"); 
 
 logo.addEventListener("click", function () {
   document.getElementById("log").textContent = "El mejor Parrilladas";
 }
 );
+
 
 inicio1.addEventListener("click", function () { 
   alert("Aca es inicio");
@@ -31,3 +31,68 @@ newForm1.addEventListener("submit", function (event) {
   }
 });
 
+localStorage.clear();
+
+let nos=document.getElementById("acercaNos")
+
+let nombreInfo="Acerca de nosotros";
+
+let nomInfo=localStorage.setItem("acerNos",nombreInfo);
+
+let nom1=localStorage.getItem("acerNos");
+
+nos.textContent = nom1;
+
+
+let comidasAPI=document.getElementById("comidasAPI");
+console.log(comidasAPI);
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+
+.then((respuesta)=>respuesta.json())
+
+.then(datos => {
+
+      datos.meals.forEach((elemento) => {
+
+          const contenedorCreado = document.createElement("div"); 
+          contenedorCreado.innerHTML = `
+          <h3>${elemento.strMeal}</h3>
+          <img src="${elemento.strMealThumb}">
+          `
+          ; 
+          comidasAPI.append(contenedorCreado);
+      });
+  
+})
+
+fetch("comidas.json")
+
+.then((respuesta)=>respuesta.json())
+
+.then(datos => {
+
+      datos.forEach((elemento) => {
+
+          const contenedorCreado = document.createElement("div"); 
+          contenedorCreado.innerHTML = `
+          <h3>${elemento.nombre}</h3>
+          `
+          ; 
+          comidasJson.append(contenedorCreado);
+})
+})
+
+const comidaId = document.getElementById("comidaId");
+
+let comidas=["arroz", "pure" , "chocolate" ]  ;
+
+comidas.forEach((elemento) => {
+
+      const contenedorCreado = document.createElement("div"); 
+      contenedorCreado.innerHTML = `
+      <h3>${elemento}</h3>
+      `
+      ; 
+      comidaId.append(contenedorCreado);
+})
